f3f9ed501cb8cb5f319159fb741948cbd6a8f89e
Md. Maynul Islam <islammaynul82@gmail.com>
2024-08-15 12:08:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-400">
+      Hello world!
+    </h1>
+    <button className="btn btn-accent">Accent</button>
+    </>
+  )
+}
+
+export default App
