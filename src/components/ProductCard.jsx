d60ad5b181f3a146144dc82bc2a89a7d0cab43bd
Md. Maynul Islam <islammaynul82@gmail.com>
2024-08-16 15:36:59 +0600

    added pagination func in product home page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,22 @@
 import PropTypes from 'prop-types';
-const ProductCard = ({item}) => {
+const ProductCard = ({ item }) => {
 
     return (
         <div>
             <div className="card border-2 border-base w-96 mt-12 p-4">
-            <figure>
-                <img className='h-[300px]'
-                    src={item.image}
-                    alt={item.name} />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">{item.name}</h2>
-                <p>{item.description}</p>
-                <p className='font-semibold'>Price: <span>{item.price}</span>$</p>
-                <p className='badge badge-outline'>{item.brand}</p>
+                <figure>
+                    <img className='h-[300px]'
+                        src={item.image}
+                        alt={item.name} />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">{item.name}</h2>
+                    <p>{item.description}</p>
+                    <p className='font-semibold'>Price: <span>{item.price}</span>$</p>
+                    <p className='badge badge-outline'>{item.brand}</p>
+                    <p className='font-semibold'>Rating: {item.ratings}</p>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,146 @@
-import ProductCards from "../components/ProductCards";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    const [itemsPerPage, setItemsPerPage] = useState(6);
+    const [count, setCount] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [categoryFilter, setCategoryFilter] = useState('')
+
+    useEffect(() => {
+        const getData = async () => {
+            const { data } = await axios(`${import.meta.env.VITE_API_URL}/all-products?page=${currentPage}&size=${itemsPerPage}&filter=`)
+            setProducts(data)
+        }
+        getData()
+    }, [currentPage, itemsPerPage])
+    useEffect(() => {
+        const getCount = async () => {
+            const { data } = await axios(`${import.meta.env.VITE_API_URL}/products-count`)
+            setCount(data.count)
+        }
+        getCount()
+    }, [])
+
+    const numberOfPages = Math.ceil(count / itemsPerPage)
+    const pages = [...Array(numberOfPages).keys()].map(element => element + 1)
+
+    //pagination button control
+    const handlePaginationButton = (v) => {
+        setCurrentPage(v)
+    }
+
     return (
         <div className="container mx-auto">
-            <ProductCards></ProductCards>
+            {/* search input */}
+            <div className="mt-8">
+                <form className="flex items-center">
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-tr-none rounded-br-none" />
+                    <input className="btn bg-red-400 rounded-tl-none rounded-bl-none" type="submit" value="Search" />
+                </form>
+            </div>
+            {/* filter */}
+            <div className="mt-4">
+                {/* brand filter */}
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn m-1">Brands</div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                    </ul>
+                </div>
+                {/* category filter */}
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn m-1">Category</div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <li><a>Phone</a></li>
+                        <li><a>Laptop</a></li>
+                        <li><a>Watch</a></li>
+                        <li><a>Tablet</a></li>
+                    </ul>
+                </div>
+                {/* Price range filter */}
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn m-1">Price Range</div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            {/* product cards */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+                {
+                    products.map(item => <ProductCard key={item?._id} item={item}></ProductCard>)
+                }
+            </div>
+            {/* Pagination Section */}
+            <div className='flex justify-center mt-12'>
+                {/* Previous Button */}
+                <button
+                    disabled={currentPage === 1}
+                    onClick={() => handlePaginationButton(currentPage - 1)}
+                    className='px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-red-400  hover:text-white'
+                >
+                    <div className='flex items-center -mx-1'>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            className='w-6 h-6 mx-1 rtl:-scale-x-100'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                        >
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                strokeWidth='2'
+                                d='M7 16l-4-4m0 0l4-4m-4 4h18'
+                            />
+                        </svg>
+
+                        <span className='mx-1'>previous</span>
+                    </div>
+                </button>
+                {/* Numbers */}
+                {pages.map(btnNum => (
+                    <button
+                        onClick={() => handlePaginationButton(btnNum)}
+                        key={btnNum}
+                        className={`hidden ${currentPage === btnNum ? 'bg-red-400 text-white' : ''
+                            } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-red-400  hover:text-white`}
+                    >
+                        {btnNum}
+                    </button>
+                ))}
+                {/* Next Button */}
+                <button
+                    disabled={currentPage === numberOfPages}
+                    onClick={() => handlePaginationButton(currentPage + 1)}
+                    className='px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-red-400 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500'
+                >
+                    <div className='flex items-center -mx-1'>
+                        <span className='mx-1'>Next</span>
+
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            className='w-6 h-6 mx-1 rtl:-scale-x-100'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                        >
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                strokeWidth='2'
+                                d='M17 8l4 4m0 0l-4 4m4-4H3'
+                            />
+                        </svg>
+                    </div>
+                </button>
+            </div>
         </div>
     );
 };
